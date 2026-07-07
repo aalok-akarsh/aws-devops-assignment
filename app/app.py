8a99c4019064c4d5e722a561d5c62547133e6f95
alokpandey. <pandeyalok3663@gmail.com.>
2026-07-07 18:47:49 +0000

    Update API response

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "AWS DevOps Assignment",
+        "message": "AWS DevOps Assignment v2",
         "status": "Running"
     })
 
